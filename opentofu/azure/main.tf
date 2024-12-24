7a780d6679b5456cdabd4b3d63a5a8a9ef6f3b06
Jimmy Lindsey <jimmyl@pacbell.net>
2024-12-23 22:15:05 -0800

    Fixed up the mash azure jobs

--- opentofu/azure/main.tf
+++ opentofu/azure/main.tf
@@ -1,101 +1,101 @@
 data "azurerm_resource_group" "rg" {
-    name = var.resourceGroupName
+  name = var.resourceGroupName
 }
 
 resource "azurerm_storage_account" "sa" {
-    name = var.storageAccountName
-    resource_group_name = data.azurerm_resource_group.rg.name
-    location = var.location
-    account_tier = "Standard"
-    account_replication_type = var.storageAccountReplicationType
+  name                     = var.storageAccountName
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = var.storageAccountReplicationType
 }
 
 resource "azurerm_storage_container" "container" {
-    name = var.containerName
-    storage_account_name = azurerm_storage_account.sa.name
-    container_access_type = var.containerAccessType
+  name                  = var.containerName
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = var.containerAccessType
 }
 
 resource "azurerm_shared_image_gallery" "computeGallery" {
-    name = var.galleryName
-    resource_group_name = data.azurerm_resource_group.rg.name
-    location = var.location
-    description = var.galleryDescription
-    sharing {
-        permission = "Community"
-        community_gallery {
-            eula = var.eula
-            prefix = var.prefix
-            publisher_email = var.publisherEmail
-            publisher_uri = var.publisherURI
-        }
+  name                = var.galleryName
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = var.location
+  description         = var.galleryDescription
+  sharing {
+    permission = "Community"
+    community_gallery {
+      eula            = var.eula
+      prefix          = var.prefix
+      publisher_email = var.publisherEmail
+      publisher_uri   = var.publisherURI
     }
+  }
 }
 
 resource "azurerm_shared_image" "armImageDefinitionV1" {
-    name = "${var.armImageDefinitionBaseName}_${var.imageDefinitionSKU}"
-    gallery_name = azurerm_shared_image_gallery.computeGallery.name
-    resource_group_name = data.azurerm_resource_group.rg.name
-    location =  var.location
-    os_type = "Linux"
-    architecture = "Arm64"
-    hyper_v_generation = "V1"
-    identifier {
-      offer = "arm1"
-      publisher = "jimmy"
-      sku = var.imageDefinitionSKU
-    }
-    trusted_launch_supported = false
-    accelerated_network_support_enabled = true
+  name                = "${var.armImageDefinitionBaseName}_${var.imageDefinitionSKU}"
+  gallery_name        = azurerm_shared_image_gallery.computeGallery.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = var.location
+  os_type             = "Linux"
+  architecture        = "Arm64"
+  hyper_v_generation  = "V1"
+  identifier {
+    offer     = "arm1"
+    publisher = "jimmy"
+    sku       = var.imageDefinitionSKU
+  }
+  trusted_launch_supported            = false
+  accelerated_network_support_enabled = true
 }
 
 resource "azurerm_shared_image" "armImageDefinitionV2" {
-    name = "${var.armImageDefinitionBaseName}_${var.imageDefinitionSKU}_${var.imageDefinitionGeneration2}"
-    gallery_name = azurerm_shared_image_gallery.computeGallery.name
-    resource_group_name = data.azurerm_resource_group.rg.name
-    location =  var.location
-    os_type = "Linux"
-    architecture = "Arm64"
-    hyper_v_generation = "V2"
-    identifier {
-      offer = "arm"
-      publisher = "jimmy"
-      sku = var.imageDefinitionSKU
-    }
-    trusted_launch_supported = true
-    accelerated_network_support_enabled = true
+  name                = "${var.armImageDefinitionBaseName}_${var.imageDefinitionSKU}_${var.imageDefinitionGeneration2}"
+  gallery_name        = azurerm_shared_image_gallery.computeGallery.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = var.location
+  os_type             = "Linux"
+  architecture        = "Arm64"
+  hyper_v_generation  = "V2"
+  identifier {
+    offer     = "arm"
+    publisher = "jimmy"
+    sku       = var.imageDefinitionSKU
+  }
+  trusted_launch_supported            = true
+  accelerated_network_support_enabled = true
 }
 
 resource "azurerm_shared_image" "x86ImageDefitionV1" {
-    name = "${var.x86ImageDefinitionBaseName}_${var.imageDefinitionSKU}"
-    gallery_name = azurerm_shared_image_gallery.computeGallery.name
-    resource_group_name = data.azurerm_resource_group.rg.name
-    location =  var.location
-    os_type = "Linux"
-    architecture = "x64"
-    hyper_v_generation = "V1"
-    identifier {
-      offer = "x86"
-      publisher = "jimmy"
-      sku = var.imageDefinitionSKU
-    }
-    trusted_launch_supported = false
-    accelerated_network_support_enabled = true
+  name                = "${var.x86ImageDefinitionBaseName}_${var.imageDefinitionSKU}"
+  gallery_name        = azurerm_shared_image_gallery.computeGallery.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = var.location
+  os_type             = "Linux"
+  architecture        = "x64"
+  hyper_v_generation  = "V1"
+  identifier {
+    offer     = "x86"
+    publisher = "jimmy"
+    sku       = var.imageDefinitionSKU
+  }
+  trusted_launch_supported            = false
+  accelerated_network_support_enabled = true
 }
 
 resource "azurerm_shared_image" "x86ImageDefitionV2" {
-    name = "${var.x86ImageDefinitionBaseName}_${var.imageDefinitionSKU}_${var.imageDefinitionGeneration2}"
-    gallery_name = azurerm_shared_image_gallery.computeGallery.name
-    resource_group_name = data.azurerm_resource_group.rg.name
-    location =  var.location
-    os_type = "Linux"
-    architecture = "x64"
-    hyper_v_generation = "V2"
-    identifier {
-      offer = "x862"
-      publisher = "jimmy"
-      sku = var.imageDefinitionSKU
-    }
-    trusted_launch_supported = true
-    accelerated_network_support_enabled = true
+  name                = "${var.x86ImageDefinitionBaseName}_${var.imageDefinitionSKU}_${var.imageDefinitionGeneration2}"
+  gallery_name        = azurerm_shared_image_gallery.computeGallery.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = var.location
+  os_type             = "Linux"
+  architecture        = "x64"
+  hyper_v_generation  = "V2"
+  identifier {
+    offer     = "x862"
+    publisher = "jimmy"
+    sku       = var.imageDefinitionSKU
+  }
+  trusted_launch_supported            = true
+  accelerated_network_support_enabled = true
 }
--- opentofu/azure/outputs.tf
+++ opentofu/azure/outputs.tf
@@ -1,27 +1,27 @@
 output "source_storage_account" {
-    value = azurerm_storage_account.sa.name
+  value = azurerm_storage_account.sa.name
 }
 
 output "source_container" {
-    value = azurerm_storage_container.container.name
+  value = azurerm_storage_container.container.name
 }
 
 output "gallery_name" {
-    value = azurerm_shared_image_gallery.computeGallery.name
+  value = azurerm_shared_image_gallery.computeGallery.name
 }
 
 output "arm_offer_id" {
-    value = var.armImageDefinitionBaseName
+  value = var.armImageDefinitionBaseName
 }
 
 output "eightySix_offer_id" {
-    value = var.x86ImageDefinitionBaseName
+  value = var.x86ImageDefinitionBaseName
 }
 
 output "image_sku" {
-    value = var.imageDefinitionSKU
+  value = var.imageDefinitionSKU
 }
 
 output "generation_id" {
-    value = "${var.imageDefinitionSKU}_${var.imageDefinitionGeneration2}"
+  value = "${var.imageDefinitionSKU}_${var.imageDefinitionGeneration2}"
 }
